f3e1cdebe41ff84891539288b0fc797e723b9412
Aaron Janes <aaron.janes@gmail.com>
2023-06-21 06:53:50 -0600

    deal function

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
-	//fmt.Println(cards)
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("======")
+	remainingCards.print()
 }
 
 /**
