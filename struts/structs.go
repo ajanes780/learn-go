77c30724ed016d754a0821834274e1e24e8c3980
Aaron Janes <aaron.janes@gmail.com>
2023-06-23 07:10:26 -0600

    more work with structs

--- struts/structs.go
+++ struts/structs.go
@@ -6,16 +6,26 @@ type person struct {
 	firstName string
 	lastName  string
 	age       int
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	phone   int
+	zipcode int
 }
 
 func main() {
 
-	alex := person{
-		firstName: "Alex",
-		lastName:  "Anderson",
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
 		age:       32,
+		contact: contactInfo{
+			email:   "jimparty@gmail.com",
+			phone:   7807201385,
+			zipcode: 90210,
+		},
 	}
-	fmt.Printf("%+v", alex)
-	alex.firstName = "Aaron"
-	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", jim)
 }
