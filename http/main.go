04bb5efeb6f21114e5762d375c91592175099cc6
Aaron Janes <aaron.janes@gmail.com>
2023-06-24 14:07:19 -0600

    switch statement

--- http/main.go
+++ http/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("https://www.google.com")
-	lw := logWriter{}
-	io.Copy(lw, resp.Body)
+	//c := resp.Header["Content-Type"]
+	ct := strings.Split(resp.Header["Content-Type"][0], ";")
+	fmt.Println(ct[0])
+	//lw := logWriter{}
+	//_, err = io.Copy(lw, resp.Body)
 
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
+	}
+
+	switch ct[0] {
+	case "text/html":
+		fmt.Println("content type is text/html")
+	case "linux":
+		fmt.Println("Go runs on Linux.")
+	default:
+		fmt.Printf("Go runs on ")
 	}
 }
 
