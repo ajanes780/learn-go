ea00b284f2ec73624fe3ce06fd718571dffd6378
Aaron Janes <aaron.janes@gmail.com>
2023-06-24 21:31:26 -0600

    assignment 2

--- assignment/main.go
+++ assignment/main.go
@@ -6,20 +6,25 @@ type shape interface {
 	getArea() float64
 }
 
-type triangle struct{}
-type square struct{}
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	sideLength float64
+}
 
 func main() {
-	t := triangle{}
-	s := square{}
+	t := triangle{base: 10, height: 10}
+	s := square{sideLength: 10}
 	fmt.Println(t.getArea())
 	fmt.Println(s.getArea())
 }
 
 func (t triangle) getArea() float64 {
-	return 0.5 * 0.5
+	return 0.5 * t.base * t.height
 }
 
 func (s square) getArea() float64 {
-	return 0.5 * 0.5
+	return s.sideLength * s.sideLength
 }
---
+++ assignment2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	f := readFile(os.Args[1])
+
+	//lw := logWriter{}
+	_, err := io.Copy(os.Stdout, f)
+	if err != nil {
+		return
+	}
+}
+
+func readFile(filename string) *os.File {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error opening file: %v", err)
+		os.Exit(1)
+	}
+	return f
+}
